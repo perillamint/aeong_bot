4bc6ef03ff2ae34945544206dc42ab95e723c764
perillamint <perillamint@gentoo.moe>
2017-04-07 21:50:21 +0900

    Tweet stream listener

--- lib/aeong/twitter_stream.ex
+++ lib/aeong/twitter_stream.ex
@@ -5,28 +5,40 @@ defmodule AeongBot.Aeong.TwitterStream do
     defstruct [:twitter]
   end
 
+  defmodule StreamListener do
+    def start(parent) do
+      ExTwitter.stream_user()
+      |> Enum.take_while(fn (data) ->
+        GenServer.cast(parent, data)
+      end)
+    end
+  end
+
   def start_link(args) do
     GenServer.start_link(__MODULE__, [args], [])
   end
 
   def init(args) do
     state = %{
+      extwitter: :undefined
     }
 
-    {:ok, state, 0}
+    spawn_link(StreamListener, :start, [self()])
+
+    {:ok, state}
   end
 
   def handle_call(_, _, state) do
     {:stop, {:error, :unknownmsg}, state}
   end
 
-  def handle_cast(_, _, state) do
-    {:stop, {:error, :unknownmsg}, state}
+  def handle_cast(msg, state) do
+    IO.inspect(msg)
+    {:noreply, state}
   end
 
   def handle_info(timeout, state) do
-    # TODO: Fill this
-    {:noreply, state, 0}
+    {:noreply, state}
   end
 
   def terminate(reason, state) do
