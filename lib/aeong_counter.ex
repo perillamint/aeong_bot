eb22acabe1fb7826bc06bd8e8a4136d49838765c
perillamint <perillamint@users.noreply.github.com>
2017-04-07 19:49:02 +0900

    Initial commit

---
+++ lib/aeong_counter.ex
@@ -0,0 +1,18 @@
+defmodule AeongCounter do
+  @moduledoc """
+  Documentation for AeongCounter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AeongCounter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
