f95f4151c104a24f5f30133ac3acf8d65f7523df
perillamint <perillamint@gentoo.moe>
2017-04-07 23:05:09 +0900

    Write to Mnesia

--- lib/aeong/aeong_counter.ex
+++ lib/aeong/aeong_counter.ex
@@ -1,6 +1,7 @@
 defmodule AeongBot.Aeong.AeongCounter do
   use GenServer
   use Amnesia
+  alias AeongBot.Mnesia.Aeong.AeongCount, as: AeongCount
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, [args], [{:name, __MODULE__}])
@@ -18,7 +19,20 @@ defmodule AeongBot.Aeong.AeongCounter do
     IO.inspect(aeong)
 
     Amnesia.transaction do
-      #
+      aeong = case AeongCount.read(aeong.user_id) do
+                nil ->
+                  %AeongCount{
+                    twitter_user_id: aeong.user_id,
+                    screen_name: aeong.screen_name,
+                    count: 0
+                  }
+                aeong_count ->
+                  aeong_count
+              end
+
+      aeong = %{aeong | count: aeong.count + 1}
+
+      AeongCount.write(aeong)
     end
 
     {:noreply, state}
